9a92af13f1e36a5023fd25c1abcc6aa73ce54d1d
Rhuam PFC <68817655+Rhuam774@users.noreply.github.com>
2021-11-26 00:01:07 -0300

    update

--- dns_brut.py
+++ dns_brut.py
@@ -1,6 +1,27 @@
 import requests
+from time import sleep
+import os
+
+try:
+    wordlist = open('wordlist.txt', 'r')
+except Exception as erro:
+    print(f'erro: {erro}')   
 
 while True:
-    web = requests.get('https://google.com')
-    print(web.status_code)
-
+    site = input("digite o dominio: http's://")
+    print('\n=============================================\n')
+    for subdomain in wordlist:
+        try:
+            montagem = 'https://'+site+subdomain
+            web = requests.get(montagem)
+            if web.status_code == 200:
+                print(web.status_code)
+                print(montagem)
+                sleep(5)
+                try:
+                    os.system('clear')
+                except:
+                    os.system('cls')
+        except Exception as erro:
+            print(f'erro: {erro}\n')
+            sleep(3)
