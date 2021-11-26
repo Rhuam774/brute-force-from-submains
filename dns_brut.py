3af8f0be4fd82b853b49df0648200dd8512d03f6
Rhuam PFC <68817655+Rhuam774@users.noreply.github.com>
2021-11-26 01:08:30 -0300

    update

--- dns_brut.py
+++ dns_brut.py
@@ -3,11 +3,15 @@ from time import sleep
 import os
 
 try:
-    wordlist = open('wordlist.txt', 'r')
+    wordlist = open('wordlist.txt', 'r+')
 except Exception as erro:
     print(f'erro: {erro}')   
 
+add_encontrado = open('subdominios_encontrado.txt', 'w')
+add_encontrado.close()
+a = 0
 while True:
+    
     site = input("digite o dominio: http's://")
     print('\n=============================================\n')
     for subdomain in wordlist:
@@ -15,13 +19,20 @@ while True:
             montagem = 'https://'+site+subdomain
             web = requests.get(montagem)
             if web.status_code == 200:
-                print(web.status_code)
-                print(montagem)
-                sleep(5)
-                try:
+                add_encontrado = open('subdominios_encontrado.txt', 'a+')
+                web.status_code = str(web.status_code)
+                add_encontrado.write(montagem+'   '+web.status_code)
+                add_encontrado.close()
+                os.system('clear')
+                print(web.status_code+'\n'+montagem+'\nAdicionado ao arquivo de texto!')
+                sleep(3)
+                os.system('clear')
+            else: 
+                print(montagem+'  NÃO CONFIRMADO!\n')
+                a += 1
+                if a == 100:
                     os.system('clear')
-                except:
-                    os.system('cls')
+                    a = 0           
         except Exception as erro:
-            print(f'erro: {erro}\n')
+             print(f'erro: {erro}\n')          
             sleep(3)
